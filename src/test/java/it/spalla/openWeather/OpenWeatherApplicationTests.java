497684b0a749b75e7ba61763f6aebb15d5c151a5
diegoSpalla <diego.1636@gmail.com>
2021-05-19 23:22:35 +0200

    Initial commit

---
+++ src/test/java/it/spalla/openWeather/OpenWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package it.spalla.openWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
