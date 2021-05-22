7746bb11406282399b65e9c340ee2d49639c509a
diegoSpalla <diego.1636@gmail.com>
2021-05-22 13:11:04 +0200

    Create interface to define needed methods for WeatherInformationService

---
+++ src/main/java/it/spalla/openWeather/service/entities/WeatherInformationService.java
@@ -0,0 +1,42 @@
+package it.spalla.openWeather.service.entities;
+
+import it.spalla.openWeather.model.City;
+import it.spalla.openWeather.model.WeatherInformation;
+
+import java.util.List;
+
+public interface WeatherInformationService
+{
+	//###########CREATE
+	
+	/**
+	 * Persist a weatherInformation in the database,
+	 * it is created in case it doesn't exists, it is updated otherwise
+	 *
+	 * @param information The weatherInformation entity
+	 * @return The entity persisted
+	 */
+	WeatherInformation persist(WeatherInformation information);
+	
+	/**
+	 * Create records with temperature information for all City
+	 * entities saved in the database
+	 *
+	 * @return The number of records created
+	 */
+	int createTemperaturesInformationForAllCities();
+	
+	/**
+	 * Create record for temperature information for a city entity
+	 *
+	 * @param city The city entity
+	 * @return True in case of success, False otherwise
+	 */
+	boolean createTemperatureInformationForCity(City city);
+	
+	//###########READ
+	
+	//###########UPDATE
+	
+	//###########DELETE
+}
