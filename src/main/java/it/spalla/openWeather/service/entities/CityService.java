867f4d28554495a7f1b12dbe3f47f54d88d827c4
diegoSpalla <diego.1636@gmail.com>
2021-05-22 13:10:20 +0200

    Create interface to define needed methods for CityService

---
+++ src/main/java/it/spalla/openWeather/service/entities/CityService.java
@@ -0,0 +1,24 @@
+package it.spalla.openWeather.service.entities;
+
+import it.spalla.openWeather.model.City;
+
+import java.util.List;
+
+public interface CityService
+{
+	//###########CREATE
+	
+	//###########READ
+	
+	/**
+	 * Find all the City entities
+	 *
+	 * @return A list of all the City entities
+	 */
+	List<City> findAll();
+	
+	//###########UPDATE
+	
+	//###########DELETE
+	
+}
